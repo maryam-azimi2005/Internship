491cf533b82ba6c106e8c35e201b637e64b3a58c
Maryam Azimi <158469406+maryam-azimi2005@users.noreply.github.com>
2026-02-19 22:07:23 +0330

    task 4: Organize posts app URL structure and add missing API routes

--- Task3/my_project/posts/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('posts/', views.post_list, name='post-list'),
-    path('authors/', views.author_list, name='author-list'),
-]
-
